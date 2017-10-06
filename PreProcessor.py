b6f95dd1000a078189eda207ec3fed942c72b5c0
kdbolton <latestthing@gmail.com>
2017-10-06 13:03:40 -0500

    fixed num args and run main

--- PreProcessor.py
+++ PreProcessor.py
@@ -1,23 +1,27 @@
 import sys
 import pandas as pd
 from pathlib import Path
+from pprint import pprint
 
 usage = 'PreProcessor.py /path/to/raw/dataset /path/to/processed/dataset'
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     print('not enough arguments\n')
     print(usage)
     sys.exit(1)
-if len(sys.argv) > 2:
+if len(sys.argv) > 3:
     print('too many arguments\n')
     print(usage)
     sys.exit(1)
 
-def main():
-    input_data_path = Path(sys.argv[1])
-    output_data_path = Path(sys.argv[2])
+def main(argv):
+    input_data_path = Path(argv[1])
+    output_data_path = Path(argv[2])
 
-    raw_input_data = pd.read_csv(input_data_path)
+    raw_input_data = pd.read_csv(input_data_path, header=None)
 
+    pprint(raw_input_data)
 
     return 0
+
+main(sys.argv)
