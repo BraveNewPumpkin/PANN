0374aba75e8b087b73ef2e7dbfe820606d79fceb
kdbolton <latestthing@gmail.com>
2017-10-12 22:25:09 -0500

    encoded data. unfinished attempt to insert new onehotencoded columns back into dataframe

--- PreProcessor.py
+++ PreProcessor.py
@@ -2,9 +2,10 @@ import sys
 import pandas as pd
 from pandas.api.types import is_string_dtype, is_numeric_dtype, is_bool_dtype
 from pathlib import Path
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder, LabelBinarizer
 from pprint import pprint
 
-usage = 'PreProcessor.py /path/to/raw/dataset /path/to/processed/dataset'
+usage = 'PreProcessor.py /path/to/raw/dataset.csv /path/to/processed/dataset.csv'
 
 if len(sys.argv) < 3:
     print('not enough arguments\n')
@@ -24,23 +25,38 @@ def main(argv):
     #clean missing and nulls
     raw_input_data.dropna(axis=0, how='any')
 
+    standard_scaler = StandardScaler()
+    label_encoder = LabelEncoder()
+
+    #normalize data
+    categorical_column_names = []
     for column_name in raw_input_data.columns:
         column = raw_input_data[column_name]
         if is_numeric_dtype(column):
-            #numericColumnHandler(raw_input_data, column_name)
-            numericColumnHandler(column)
+            raw_input_data[column_name] = standard_scaler.fit_transform(raw_input_data[[column_name]].as_matrix())
         elif is_string_dtype(column):
-            stringColumnHandler(column)
+            raw_input_data[column_name] = label_encoder.fit_transform(raw_input_data[column_name])
+            categorical_column_names.append(column_name)
         elif not is_bool_dtype(column):
             raise TypeError('column is of unhandleable dtype: ', column.dtype)
 
+    one_hot_encoder = OneHotEncoder(categorical_features=categorical_column_names, sparse=False)
+    encoded_array = one_hot_encoder.fit_transform(raw_input_data)
+
+    #remove old categorical columns
+    raw_input_data.drop(categorical_column_names, axis=1, inplace=True)
+    #add newly encoded columns to dataframe
+    new_columns_dict = {}
+    #construct dictionary with keys of new column names (indicies starting at 1 past the last index of dataframe)
+    # and values of the new encoded columns
+    for i in range(0, encoded_array.shape[1]):
+       new_columns_dict[raw_input_data.shape[1] + i + 1] = encoded_array.T[i]
+    pprint(new_columns_dict)
+    #add new columns to dataframe
+    raw_input_data.assign(new_columns_dict)
+
+#    raw_input_data.to_csv(output_data_path)
 
     return 0
-
-def numericColumnHandler(column):
-    print('numeric')
-
-def stringColumnHandler(column):
-    print('string')
 
 main(sys.argv)
