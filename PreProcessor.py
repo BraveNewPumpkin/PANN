4ec0aa7407dc79f5f04e9f4c26af188f508f5087
kdbolton <latestthing@gmail.com>
2017-10-13 00:29:27 -0500

    write to csv

--- PreProcessor.py
+++ PreProcessor.py
@@ -20,43 +20,53 @@ def main(argv):
     input_data_path = Path(argv[1])
     output_data_path = Path(argv[2])
 
-    raw_input_data = pd.read_csv(input_data_path, header=None)
+    data = pd.read_csv(input_data_path, header=None)
 
     #clean missing and nulls
-    raw_input_data.dropna(axis=0, how='any')
+    data.dropna(axis=0, how='any')
 
     standard_scaler = StandardScaler()
     label_encoder = LabelEncoder()
 
+    #temporarily save and remove classifier column from dataframe
+    classifier_name = data.columns[-1]
+    classifier_column = data[classifier_name]
+    data.drop(classifier_name, axis=1, inplace=True)
+
+
     #normalize data
     categorical_column_names = []
-    for column_name in raw_input_data.columns:
-        column = raw_input_data[column_name]
+    for column_name in data.columns:
+        column = data[column_name]
         if is_numeric_dtype(column):
-            raw_input_data[column_name] = standard_scaler.fit_transform(raw_input_data[[column_name]].as_matrix())
+            data[column_name] = standard_scaler.fit_transform(data[[column_name]].as_matrix())
         elif is_string_dtype(column):
-            raw_input_data[column_name] = label_encoder.fit_transform(raw_input_data[column_name])
+            data[column_name] = label_encoder.fit_transform(data[column_name])
             categorical_column_names.append(column_name)
         elif not is_bool_dtype(column):
             raise TypeError('column is of unhandleable dtype: ', column.dtype)
 
     one_hot_encoder = OneHotEncoder(categorical_features=categorical_column_names, sparse=False)
-    encoded_array = one_hot_encoder.fit_transform(raw_input_data)
+    encoded_array = one_hot_encoder.fit_transform(data)
 
     #remove old categorical columns
-    raw_input_data.drop(categorical_column_names, axis=1, inplace=True)
+    data.drop(categorical_column_names, axis=1, inplace=True)
     #add newly encoded columns to dataframe
     new_columns_dict = {}
     #construct dictionary with keys of new column names (indicies starting at 1 past the last index of dataframe)
     # and values of the new encoded columns
     for i in range(0, encoded_array.shape[1]):
-        column_name = str(raw_input_data.shape[1] + i + 1)
+        column_name = str(data.shape[1] + i + 1)
         column_data = encoded_array.T[i]
         new_columns_dict[column_name] = column_data
     #add new columns to dataframe
-    raw_input_data = raw_input_data.assign(**new_columns_dict)
+    data = data.assign(**new_columns_dict)
 
-#    raw_input_data.to_csv(output_data_path)
+    #add classifier column back on
+    data.assign(classifier=classifier_column)
+
+    with output_data_path.open(mode='w') as output_data_stream:
+        data.to_csv(output_data_stream, header=None)
 
     return 0
 
