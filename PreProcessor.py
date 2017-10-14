620e65f5e1a945cff3c8b28d7efc0749b5729450
kdbolton <latestthing@gmail.com>
2017-10-13 20:41:30 -0500

    classifier columns are named "classifier" or "classifier_*" where "*" is one of the class values if there were multiple classes

--- PreProcessor.py
+++ PreProcessor.py
@@ -26,47 +26,34 @@ def main(argv):
     data.dropna(axis=0, how='any')
 
     standard_scaler = StandardScaler()
-    label_encoder = LabelEncoder()
+    label_binarizer = LabelBinarizer()
 
-    #temporarily save and remove classifier column from dataframe
-    classifier_name = data.columns[-1]
-    classifier_column = data[classifier_name]
-    data.drop(classifier_name, axis=1, inplace=True)
+    data.rename(index=str, columns={data.columns[-1]: 'classifier'}, inplace=True)
 
-
-    #normalize data
-    categorical_column_names = []
+   #normalize data
     for column_name in data.columns:
         column = data[column_name]
         if is_numeric_dtype(column):
             data[column_name] = standard_scaler.fit_transform(data[[column_name]].as_matrix())
         elif is_string_dtype(column):
-            data[column_name] = label_encoder.fit_transform(data[column_name])
-            categorical_column_names.append(column_name)
+            encoded_array = label_binarizer.fit_transform(column)
+            #remove old categorical columns
+            data.drop(column_name, axis=1, inplace=True)
+            #add newly encoded columns to dataframe
+            new_columns_dict = {}
+            #construct dictionary with keys of new column names (indicies starting at 1 past the last index of dataframe)
+            # and values of the new encoded columns
+            for i in range(0, encoded_array.shape[1]):
+                encoded_column_name = str(column_name) + '=' + str(label_binarizer.classes_[i])
+                column_data = encoded_array.T[i]
+                new_columns_dict[encoded_column_name] = column_data
+            #add new columns to dataframe
+            data = data.assign(**new_columns_dict)
         elif not is_bool_dtype(column):
             raise TypeError('column is of unhandleable dtype: ', column.dtype)
 
-    one_hot_encoder = OneHotEncoder(categorical_features=categorical_column_names, sparse=False)
-    encoded_array = one_hot_encoder.fit_transform(data)
-
-    #remove old categorical columns
-    data.drop(categorical_column_names, axis=1, inplace=True)
-    #add newly encoded columns to dataframe
-    new_columns_dict = {}
-    #construct dictionary with keys of new column names (indicies starting at 1 past the last index of dataframe)
-    # and values of the new encoded columns
-    for i in range(0, encoded_array.shape[1]):
-        column_name = str(data.shape[1] + i + 1)
-        column_data = encoded_array.T[i]
-        new_columns_dict[column_name] = column_data
-    #add new columns to dataframe
-    data = data.assign(**new_columns_dict)
-
-    #add classifier column back on
-    data.insert(loc=data.shape[1], column='classifier', value=classifier_column)
-
     with output_data_path.open(mode='w') as output_data_stream:
-        data.to_csv(output_data_stream)
+        data.to_csv(output_data_stream)#, header=None)
 
     return 0
 
